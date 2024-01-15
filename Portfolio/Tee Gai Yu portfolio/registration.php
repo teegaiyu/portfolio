fd37b2e63477f0c9cc532c9ad31dc55eb21be913
teegaiyu <134262481+teegaiyu@users.noreply.github.com>
2024-01-15 20:50:05 +0800

    Add files via upload

---
+++ Portfolio/Tee Gai Yu portfolio/registration.php
@@ -0,0 +1,33 @@
+<?php 
+         
+		 $con = mysqli_connect("localhost","root","","userdb") or die("Couldn't connect");
+         if(isset($_POST['submit'])){
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+         //verifying the unique email
+
+         $verify_query = mysqli_query($con,"SELECT email FROM users WHERE email='$email'");
+
+         if(mysqli_num_rows($verify_query) !=0 ){
+            echo "<div class='message'>
+                      <p>This email is used, Try another One Please!</p>
+                  </div> <br>";
+            echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+         }
+         else{
+
+            mysqli_query($con,"INSERT INTO users(username,email,password) VALUES('$username','$email','$password')") or die("Erroe Occured");
+
+            echo "<div class='message'>
+                      <p>Registration successfully!</p>
+                  </div> <br>";
+            echo "<a href='login.html'><button class='btn'>Login Now</button>";
+         
+
+         }
+
+         }else{}
+         
+        ?>
